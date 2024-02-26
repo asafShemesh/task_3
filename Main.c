02687b95ee82a2d82043fb39de14bc19f5b7552e
asaf <asaf0604@gmail.com>
2024-02-26 22:54:55 +0200

    bla bla bla

--- Main.c
+++ Main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "StrList.h"
+
+char *inputString(FILE *fp, size_t size)
+{
+    char *str;
+    int ch;
+    size_t len = 0;
+    str = realloc(NULL, sizeof(*str) * size);
+    if (!str)
+        return str;
+    while (EOF != (ch = fgetc(fp)) && ch != '\n')
+    {
+        str[len++] = ch;
+        if (len == size)
+        {
+            str = realloc(str, sizeof(*str) * (size += 16));
+            if (!str)
+                return str;
+        }
+    }
+    str[len++] = '\0';
+
+    return realloc(str, sizeof(*str) * len);
+}
 
 int main()
 {
@@ -13,11 +37,16 @@ int main()
         {
             int index;
             scanf("%d", &index);
-            for (int i = 0; i < index; i++)
+
+            char *input = inputString(stdin, 100);
+            char *token = strtok(input, " ");
+            int wordCount = 0;
+
+            while (token != NULL && wordCount < index)
             {
-                char input[1000];
-                scanf("%s", input);
-                StrList_insertLast(list, input);
+                StrList_insertLast(list, token);
+                token = strtok(NULL, " ");
+                wordCount++;
             }
         }
 
@@ -25,7 +54,7 @@ int main()
         {
             int index;
             scanf("%d", &index);
-            char userInput[50];
+            char *userInput = inputString(stdin, 100);
             scanf("%s", userInput);
             StrList_insertAt(list, userInput, index);
         }
@@ -52,14 +81,14 @@ int main()
         }
         else if (num == 7)
         {
-            char userInput[50];
+            char *userInput = inputString(stdin, 100);
             scanf("%s", userInput);
             printf("%d", StrList_count(list, userInput));
             printf("\n");
         }
         else if (num == 8)
         {
-            char userInput[50];
+            char *userInput = inputString(stdin, 100);
             scanf("%s", userInput);
             StrList_remove(list, userInput);
         }
